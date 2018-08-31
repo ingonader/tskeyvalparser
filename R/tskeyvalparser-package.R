399931bbd51060f90d7b920176c7ddf5a17bab15
Ingo Nader <ingo.nader@unbelievable-machine.com>
2018-08-31 08:34:55 +0200

    Add description file to fix warning about global variable '.'

---
+++ R/tskeyvalparser-package.R
@@ -0,0 +1,15 @@
+#' \code{tskeyvalparser} package
+#'
+#' Parses key-value pairs in a time-series text file.
+#'
+#' @docType package
+#' @name tskeyvalparser
+#' @importFrom magrittr %>%
+#' @keywords internal
+"_PACKAGE"
+NULL
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if(getRversion() >= "2.15.1") utils::globalVariables(c("."))
+
+
