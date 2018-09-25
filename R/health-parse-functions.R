3570df87d020d4cd1b3e33600ea8864f5a2897d8
Ingo Nader <ingo.nader@unbelievable-machine.com>
2018-09-25 18:59:53 +0200

    Remove function get_path() (moved to health-parse project)

--- R/health-parse-functions.R
+++ R/health-parse-functions.R
@@ -1,27 +1,3 @@
-#' Get paths needed for script
-#'
-#' @return List with elements
-#'   \code{base} (path of the current project),
-#'   \code{r} (path containing r script files),
-#'   \code{dat} (path containing the data files),
-#' @examples
-#'   path <- get_paths()
-#' @export
-#'
-get_paths <- function() {
-  path <- list()
-  ## get dropbox path relative to current user:
-  path$base <- file.path(
-    Sys.getenv("HOME"),   ## home dir of current user
-    "Dropbox/lists"       ## Dropbox directory
-  )
-  ## define script and data paths relative to raw path:
-  ## (currently not used)
-  path$r <- file.path(path$base, "")
-  path$dat <- file.path(path$base, "")
-  return(path)
-}
-
 #' Read health files.
 #'
 #' All files that correspond to the regular expression will be read and
